c7cea499d00bdce7a0bf1b32b62685cf97e24b46
Your Name <you@example.com>
2021-06-01 00:06:42 +1000

    move path

--- Lambda function.py
+++ Lambda function.py
@@ -2,74 +2,158 @@ import json
 import boto3
 import cv2
 import numpy as np
-import json
-from urllib.parse import unquote_plus
+import urllib.parse
+import urllib.request
 import time
-from os import listdir
+
+s3 = boto3.client('s3')
+dynamodb = boto3.client('dynamodb')
+yolo_bucket = 'imageyolo'
+confthres = 0.3
+nmsthres = 0.1
+
+
+def get_labels(bucket_name):
+    # load the COCO class labels our YOLO model was trained on
+    LABELS = s3.get_object(Bucket=bucket_name, Key="coco.names")["Body"].read().decode("utf-8").strip().split("\n")
+    return LABELS
+
+def get_weights(bucket_name):
+    # derive the paths to the YOLO weights and model configuration
+    weights = s3.get_object(Bucket=bucket_name, Key="yolov3.weights")["Body"].read()
+    return weights
+
+def get_config(bucket_name):
+    config = s3.get_object(Bucket=bucket_name, Key="yolov3.cfg")["Body"].read()
+    return config
+
+def load_model(config,weights):
+    # load our YOLO object detector trained on COCO dataset (80 classes)
+    print("[INFO] loading YOLO from disk...")
+    net = cv2.dnn.readNetFromDarknet(config, weights)
+    return net
+
+def do_prediction(image,net,LABELS):
+
+    (H, W) = image.shape[:2]
+    # determine only the *output* layer names that we need from YOLO
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    # construct a blob from the input image and then perform a forward
+    # pass of the YOLO object detector, giving us our bounding boxes and
+    # associated probabilities
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+                                 swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    # print(layerOutputs)
+    end = time.time()
+    #
+    # # show timing information on YOLO
+    # print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+    # initialize our lists of detected bounding boxes, confidences, and
+    # class IDs, respectively
+    boxes = []
+    confidences = []
+    classIDs = []
+    objects = []
+    tags = []
+
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability) of
+            # the current object detection
+            scores = detection[5:]
+            # print(scores)
+            classID = np.argmax(scores)
+            # print(classID)
+            confidence = scores[classID]
+
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if confidence > confthres:
+                # scale the bounding box coordinates back relative to the
+                # size of the image, keeping in mind that YOLO actually
+                # returns the center (x, y)-coordinates of the bounding
+                # box followed by the boxes' width and height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                # use the center (x, y)-coordinates to derive the top and
+                # and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                # update our list of bounding box coordinates, confidences,
+                # and class IDs
+                boxes.append([x, y, int(width), int(height)])
+
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    # apply non-maxima suppression to suppress weak, overlapping bounding boxes
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, confthres,
+                            nmsthres)
+
+    # TODO Prepare the output as required to the assignment specification
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            print("detected item:{}".format(LABELS[classIDs[i]]))
+
+            each_object = LABELS[classIDs[i]]
+            objects.append(each_object)
+    objects = list(set(objects))
+    for each_object in objects:
+        tags.append({"S": each_object})
+
+    return tags
 
 def lambda_handler(event, context):
-    key = event['Records'][0]['s3']['object']['key']
-    etag = event['Records'][0]['s3']['object']['eTag']
-    etag = '"{}"'.format(etag)
-    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    # etag = event['Records'][0]['s3']['object']['eTag']
+    # etag = '"{}"'.format(etag)
+    bucket = event['Records'][0]['s3']['bucket']['name']
     user_identity = event['Records'][0]['userIdentity']['principalId']
-    key = unquote_plus(key)
-    s3_client = boto3.client('s3')
-    db_client = boto3.client('dynamodb')
-    table_name = 'imageDB'
+    table = "image_db"
+
+    url = s3.generate_presigned_url(
+        ClientMethod='get_object',
+        Params={
+            'Bucket': bucket,
+            'Key': key
+        }
+    )
 
     # load yolo3
-    yolo_bucket = 'imageyolo' 
-    weight_yolo3 = s3_client.get_object(Bucket=yolo_bucket, Key='yolov3.weights')
-    config_yolo3 = s3_client.get_object(Bucket=yolo_bucket, Key='yolov3.cfg')
-    coco_yolo3 = s3_client.get_object(Bucket=yolo_bucket, Key='coco.names')
-    labels = coco_yolo3['Body'].read().decode('utf8').strip().split('\n')
-    
-    # download image
-    image_tmp_path = '/tmp/' + key.split('/')[-1]
-    s3_client.download_file(Bucket = bucket_name, Key = key, Filename = image_tmp_path)
+    Lables = get_labels(yolo_bucket)
+    CFG = get_config(yolo_bucket)
+    Weights = get_weights(yolo_bucket)
+    # load the neural net.  Should be local to this method as its multi-threaded endpoint
+    nets = load_model(CFG, Weights)
 
     # read image
-    nparray = cv2.imread(image_tmp_path)
-    load_yolov3 = cv2.dnn.readNetFromDarknet(config_yolo3['Body'].read(), weight_yolo3['Body'].read())
-    yolo_layer_name = load_yolov3.getLayerNames()
-    yolo_layer_name = [yolo_layer_name[i[0] - 1] for i in load_yolov3.getUnconnectedOutLayers()]
-    blob = cv2.dnn.blobFromImage(nparray, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-    load_yolov3.setInput(blob)
-    start_time = time.time()
-    compute_image = load_yolov3.forward(yolo_layer_name)
-    end = time.time()
-    detection_result_list = []
-    confidence_list = []
-    getclass_list = []
-    for output in compute_image:
-        for detection in output:
-            scores = detection[5:]
-            getclass = np.argmax(scores)
+    # nparray = cv2.imread(image_tmp_path)
+    req = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(req.read()),dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-            confidence = scores[getclass]
-            if confidence > 0.15:
+    tags = do_prediction(image, nets, Lables)
 
-                confidence_list.append(float(confidence))
-                getclass_list.append(getclass)
-                
-    for i in range(len(getclass_list)):
-        detection_result = labels[getclass_list[i]]
-        detection_result_list.append(detection_result)
-    detection_result_list = list(set(detection_result_list))
-    print(detection_result_list)
+    url_clean = urllib.parse.urlunsplit(urllib.parse.urlsplit(url)._replace(query="", fragment=""))
 
-    item = {}
-    list_tags = []
-    for tag in detection_result_list:
-        tag_item = {}
-        tag_item["S"] = tag
-        list_tags.append(tag_item)
-    print(list_tags)
-
-    item['tags'] = {"L" : list_tags}
-    item['etag'] = {"S" : etag}
-    item['user'] = {"S" : user_identity}
-
-    response = db_client.put_item(TableName = table_name, Item = item)
+    response = dynamodb.put_item(
+        TableName=table,
+        Item={
+            "url": {"S": url_clean},
+            "tags": {"L": tags},
+            "user": {"S": user_identity}
+        }
+        )
     return response
